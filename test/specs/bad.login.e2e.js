5b6225e384b2b5bb504886f7f19e0834f4c21564
SantiagoGonzalez97 <santiagogonzalez97@hotmail.com>
2022-11-29 20:49:16 -0300

    BaSP-W15: Uncomment code of last week

--- test/specs/bad.login.e2e.js
+++ test/specs/bad.login.e2e.js
@@ -1,19 +1,19 @@
 import LoginPage from  '../pageobjects/login.page';
 
 
-// describe('My Login application', () => {
-//     beforeAll('Navigate to url', () => {
-//         browser.url("https://www.saucedemo.com/");
-//     })
-//     it('Should not login with empty fields', async () => {
-//         await LoginPage.inputUsername.setValue("");
-//         await LoginPage.inputPassword.setValue("");
-//         await LoginPage.btnLogin.click();
-//         await LoginPage.errorMsg.waitForDisplayed({timeout: 10000});
-//         await expect(LoginPage.errorMsg).toBeDisplayed();
-//         await expect(LoginPage.errorMsg).toHaveText("Epic sadface: Username is required");
-//         await expect(LoginPage.btnLogin).toBeClickable();
-//     });
-// });
+describe('My Login application', () => {
+    beforeAll('Navigate to url', () => {
+        browser.url("https://www.saucedemo.com/");
+    })
+    it('Should not login with empty fields', async () => {
+        await LoginPage.inputUsername.setValue("");
+        await LoginPage.inputPassword.setValue("");
+        await LoginPage.btnLogin.click();
+        await LoginPage.errorMsg.waitForDisplayed({timeout: 10000});
+        await expect(LoginPage.errorMsg).toBeDisplayed();
+        await expect(LoginPage.errorMsg).toHaveText("Epic sadface: Username is required");
+        await expect(LoginPage.btnLogin).toBeClickable();
+    });
+});
 
 
--- test/specs/standard.user.e2e.js
+++ test/specs/standard.user.e2e.js
@@ -1,50 +1,50 @@
-// import LoginPage from "../pageobjects/login.page";
-// import HomePage from "../pageobjects/home.page";
+import LoginPage from "../pageobjects/login.page";
+import HomePage from "../pageobjects/home.page";
 
 
-// describe('When login whit standard user', () => {
-//     beforeAll('Navigate to url', () => {
-//         browser.url("https://www.saucedemo.com/");
-//     })
-//     it('Should not login whit invalid password', async () => {
-//         await LoginPage.inputUsername.setValue("standard_user");
-//         await LoginPage.inputPassword.setValue("test");
-//         await LoginPage.btnLogin.click();
-//         await LoginPage.errorMsg.waitForDisplayed({timeout: 10000});
-//         await expect(LoginPage.inputUsername).toHaveValueContaining('standard_user');
-//         await expect(LoginPage.inputPassword).toHaveValueContaining('test')
-//         await expect(LoginPage.errorMsg).toBeDisplayed();
-//         await expect(LoginPage.errorMsg).toHaveText("Epic sadface: Username and password do not match any user in this service");
-//         await expect(LoginPage.btnLogin).toBeClickable();
-//     })
-//     it('Should not login whit invalid username', async () => {
-//         await LoginPage.inputUsername.setValue("test");
-//         await LoginPage.inputPassword.setValue("secret_sauce");
-//         await LoginPage.btnLogin.click();
-//         await LoginPage.errorMsg.waitForDisplayed({timeout: 10000});
-//         await expect(LoginPage.inputUsername).toHaveValueContaining('test');
-//         await expect(LoginPage.inputPassword).toHaveValueContaining('secret_sauce');
-//         await expect(LoginPage.errorMsg).toBeDisplayed();
-//         await expect(LoginPage.errorMsg).toHaveText("Epic sadface: Username and password do not match any user in this service");
-//         await expect(LoginPage.btnLogin).toBeClickable();
-//     })
-//     it('Should login whit validate credentials', async () => {
-//         await LoginPage.login('standard_user', 'secret_sauce');
-//     })
-//     it('Should have a title when logged in the main page', async () => {
-//         await expect(HomePage.titlePage).toHaveTitle('Swag Labs');
-//     })
-//     it('Should redirect to inventory URL when logged', async () => {
-//         await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
-//     })
-//     it('Should see in the inventory products like this', async () => {
-//         await expect(HomePage.productImage).toHaveAttr('src', 'https://www.saucedemo.com/static/media/sauce-backpack-1200x1500.34e7aa42.jpg');
-//     })
-//     it('Should log out', async () => {
-//         await HomePage.btnBurger.waitForDisplayed({timeout: 5000});
-//         await HomePage.btnBurger.click();
-//         await HomePage.btnLogout.waitForDisplayed({timeout: 5000});
-//         await HomePage.btnLogout.click();
-//         await expect(browser).toHaveUrl('https://www.saucedemo.com/');
-//     })
-// });
+describe('When login whit standard user', () => {
+    beforeAll('Navigate to url', () => {
+        browser.url("https://www.saucedemo.com/");
+    })
+    it('Should not login whit invalid password', async () => {
+        await LoginPage.inputUsername.setValue("standard_user");
+        await LoginPage.inputPassword.setValue("test");
+        await LoginPage.btnLogin.click();
+        await LoginPage.errorMsg.waitForDisplayed({timeout: 10000});
+        await expect(LoginPage.inputUsername).toHaveValueContaining('standard_user');
+        await expect(LoginPage.inputPassword).toHaveValueContaining('test')
+        await expect(LoginPage.errorMsg).toBeDisplayed();
+        await expect(LoginPage.errorMsg).toHaveText("Epic sadface: Username and password do not match any user in this service");
+        await expect(LoginPage.btnLogin).toBeClickable();
+    })
+    it('Should not login whit invalid username', async () => {
+        await LoginPage.inputUsername.setValue("test");
+        await LoginPage.inputPassword.setValue("secret_sauce");
+        await LoginPage.btnLogin.click();
+        await LoginPage.errorMsg.waitForDisplayed({timeout: 10000});
+        await expect(LoginPage.inputUsername).toHaveValueContaining('test');
+        await expect(LoginPage.inputPassword).toHaveValueContaining('secret_sauce');
+        await expect(LoginPage.errorMsg).toBeDisplayed();
+        await expect(LoginPage.errorMsg).toHaveText("Epic sadface: Username and password do not match any user in this service");
+        await expect(LoginPage.btnLogin).toBeClickable();
+    })
+    it('Should login whit validate credentials', async () => {
+        await LoginPage.login('standard_user', 'secret_sauce');
+    })
+    it('Should have a title when logged in the main page', async () => {
+        await expect(HomePage.titlePage).toHaveTitle('Swag Labs');
+    })
+    it('Should redirect to inventory URL when logged', async () => {
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+    })
+    it('Should see in the inventory products like this', async () => {
+        await expect(HomePage.productImage).toHaveAttr('src', 'https://www.saucedemo.com/static/media/sauce-backpack-1200x1500.34e7aa42.jpg');
+    })
+    it('Should log out', async () => {
+        await HomePage.btnBurger.waitForDisplayed({timeout: 5000});
+        await HomePage.btnBurger.click();
+        await HomePage.btnLogout.waitForDisplayed({timeout: 5000});
+        await HomePage.btnLogout.click();
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/');
+    })
+});
